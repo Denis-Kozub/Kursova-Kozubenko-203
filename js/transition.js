654f7cf4669de62a75c43923b231ddebc99ac277
Denis_Kozub <kozubenkodennis@gmail.com>
2023-11-16 09:49:56 +0200

    Add helping form

--- js/transition.js
+++ js/transition.js
@@ -2,6 +2,14 @@ document.getElementById("donatebtn").addEventListener("click", function() {
     window.location.href = "donation.html";
 });
 
+function showForm() {
+    document.getElementById("overlay").style.display = "flex";
+  }
+  
+  function hideForm() {
+    document.getElementById("overlay").style.display = "none";
+  }
+
 function validateEmail() {
     var emailInput = document.getElementById('emailInput');
     var emailValue = emailInput.value;
